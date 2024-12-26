3477ba68830d5f63d6e2d0450e94bd0903f910c4
Alia <ashouriali94@gmail.com>
2024-12-26 11:20:29 +0330

    Set up project.

---
+++ src/main/kotlin/com/example/separation/SeparationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.separation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeparationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeparationApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/separation/SeparationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.separation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeparationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
